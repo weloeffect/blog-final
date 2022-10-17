15f41385a2d2807cecf0e118ac562d46857f9f2e
Walid Mohamed <walidridge1@gmail.com>
2022-10-17 15:43:06 +0300

    removed node_modules

--- frontend/src/axioss.js
+++ frontend/src/axioss.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    // baseURL: "http://localhost:5000/"
-    baseURL: "https://blog-final101.herokuapp.com/"
+    baseURL: "http://localhost:5000/"
+    // baseURL: "https://blog-final101.herokuapp.com/"
 })
